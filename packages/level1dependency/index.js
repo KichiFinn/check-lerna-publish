0f7ac75427de7932878a9926e1a58ffb6f26ae8a
hbkcntt@gmail.com <hbkcntt@gmail.com>
2020-12-25 12:11:08 +0700

    - testupdate

--- packages/level1dependency/index.js
+++ packages/level1dependency/index.js
@@ -1,6 +1,6 @@
 import level2Func from "@KichiFinn/level2dependency"
 
 export function level1Func() {
-  console.log("Hello from level1Func");
+  console.log("Hello from level1Func update 3");
   level2Func();
 }
